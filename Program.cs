e6f53bb26b8b5e92a67535ce5eec6271c9fded75
Ravi Gupta <ravigupta@microsoft.com>
2024-01-23 16:36:23 +0530

    demo project

---
+++ Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.Extensions.Logging;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+var dynamicPagesPath = Path.Combine(AppContext.BaseDirectory, "MyPages");
+var dynamicPagesRelativePath = Path.GetRelativePath(Directory.GetCurrentDirectory(), dynamicPagesPath).Replace("\\", "/");
+
+Console.WriteLine($"Dynamic Path: {dynamicPagesRelativePath}");
+
+// Add services to the container.
+builder.Services.AddRazorPages().AddRazorPagesOptions(options =>
+{
+    options.RootDirectory = $"/{dynamicPagesRelativePath}";
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
